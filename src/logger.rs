6f5c699fec5e465f2b997bbc7be7fd92cc115c79
André Luis (devkcud) <andrescalisejr@gmail.com>
2023-07-05 20:30:15 -0300

    chore: New logging format

--- src/logger.rs
+++ src/logger.rs
@@ -2,29 +2,29 @@ use chrono::prelude::*;
 use colored::Colorize;
 
 fn get_current_time() -> String {
-    return format!("{}{}{}", "[".black(), Local::now().format("%H:%M:%S").to_string(), "]".black());
+    return format!("{}", Local::now().format("%H:%M:%S").to_string().dimmed());
 }
 
 pub struct Logger;
 
 impl Logger {
     pub fn info(what: &str) {
-        println!("{}  {} {} {}", get_current_time(), "INFO".bright_green().bold(), ":".black(), what);
+        println!("{}  {} {}", get_current_time(), "INFO".bright_green().bold(), what);
     }
 
     pub fn log(what: &str) {
-        println!("{}   {} {} {}", get_current_time(), "LOG".bright_blue().bold(), ":".black(), what);
+        println!("{}   {} {}", get_current_time(), "LOG".bright_blue().bold(), what);
     }
 
     pub fn warn(what: &str) {
-        println!("{}  {} {} {}", get_current_time(), "WARN".bright_yellow().bold(), ":".black(), what);
+        println!("{}  {} {}", get_current_time(), "WARN".bright_yellow().bold(), what);
     }
 
     pub fn error(what: &str, exit: i32) {
-        println!("{} {} {} {}", get_current_time(), "ERROR".bright_red().bold(), ":".black(), what);
+        println!("{} {} {}", get_current_time(), "ERROR".bright_red().bold(), what);
 
         if exit != 0 {
-            Logger::error("Exiting...", 0);
+            Logger::info("Exiting...");
             std::process::exit(exit);
         }
     }
