176e153b73b6c5b62f6a28f3639143f7d83ef840
André Luis <andrescalisejr@gmail.com>
2023-07-06 16:43:52 -0300

    fix: Remove unwraps '-'

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,21 @@ mod logger;
 use logger::Logger;
 
 fn main() {
-    baker::BakFile::new();
-    let bak = baker::BakFile::content().unwrap();
+    match baker::BakFile::new() {
+        Ok(_) => (),
+        Err(e) => {
+            Logger::error(&format!("{e}"));
+            std::process::exit(1);
+        },
+    };
+
+    let bak = match baker::BakFile::content() {
+        Ok(o) => o,
+        Err(e) => {
+            Logger::error(&format!("{e}"));
+            std::process::exit(1);
+        },
+    };
+
+    Logger::print(&bak);
 }
