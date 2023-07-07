2d31996413b9770eec3ef4c90f39ca7254a2cbcf
André Luis <andrescalisejr@gmail.com>
2023-07-06 21:25:03 -0300

    feat(RunRule): Run commands

--- src/ruler.rs
+++ src/ruler.rs
@@ -1,39 +1,59 @@
-use std::io;
+use std::{io, process::Command};
 use colored::Colorize;
 use itertools::Itertools;
 use regex::Regex;
 
 use crate::{baker::BakFile, logger::Logger};
 
-#[derive(Clone, Hash, PartialEq, Eq)]
+fn run_commands(commands: Vec<String>) -> () {
+    for command in commands.iter() {
+        match Command::new("sh").arg("-c").arg(command).output() {
+            Ok(o) => {
+                if !o.stdout.is_empty() {
+                    Logger::print(&String::from_utf8_lossy(&o.stdout));
+                }
+
+                if !o.stderr.is_empty() {
+                    Logger::print(&String::from_utf8_lossy(&o.stderr));
+                }
+            },
+
+            Err(e) => Logger::error(&e.to_string()),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
 pub struct SetRule {
     pub name: String,
     pub commands: Vec<String>,
     pub is_default: bool,
 }
 
-#[derive(Clone, Hash, PartialEq, Eq)]
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
 pub struct RunRule {
     pub to_run: Vec<SetRule>,
 }
 
-pub struct Ruler;
+pub struct Ruler {
+    file_content: String,
+    set_rules: Vec<SetRule>,
+}
 
 impl Ruler {
-    pub fn lookup_set_rules(bakfile: BakFile) -> io::Result<Vec<SetRule>> {
-        let content = bakfile.read()?;
+    pub fn new(bakfile: BakFile) -> io::Result<Self> {
+        let file_content = bakfile.read()?;
 
         let regex = Regex::new(r"(?m)^\$set.*$").unwrap();
-        let captures = regex.captures_iter(&content);
+        let name_regex = Regex::new(r"[^a-zA-Z0-9]").unwrap();
+        let captures = regex.captures_iter(&file_content);
 
         let mut rules: Vec<SetRule> = Vec::new();
-
-        let name_regex = Regex::new(r"[^a-zA-Z0-9]").unwrap();
 
         for capture in captures {
             let capture = capture[0].trim();
 
-            let commands = content
+            let commands = file_content
                 .lines()
                 .skip_while(|&x| x != capture)
                 .skip(1)
@@ -41,7 +61,7 @@ impl Ruler {
                 .filter_map(|x| if x.is_empty() { None } else { Some(x.trim().to_string()) })
                 .collect::<Vec<String>>();
 
-            let curline = content.lines().position(|x| x == capture).unwrap() + 1;
+            let curline = file_content.lines().position(|x| x == capture).unwrap() + 1;
 
             let mut arguments = capture.split_whitespace().collect::<Vec<&str>>();
             arguments.remove(0);
@@ -64,28 +84,31 @@ impl Ruler {
             }
         }
 
-        return Ok(rules.into_iter().unique().collect_vec());
+        return Ok(Self {
+            file_content,
+            set_rules: rules.into_iter().unique().collect_vec(),
+        });
     }
 
-    pub fn lookup_run_rules(bakfile: BakFile) -> io::Result<()> {
-        let content = bakfile.read()?;
-
+    pub fn setup_run(&self) -> io::Result<()> {
         let regex = Regex::new(r"(?m)^\$run.*$").unwrap();
-        let captures = regex.captures_iter(&content);
+        let captures = regex.captures_iter(&self.file_content);
 
         for capture in captures {
             let mut rules_to_run = capture[0].split_whitespace().collect::<Vec<&str>>();
             rules_to_run.remove(0);
 
             if rules_to_run.len() == 0 {
-                // TODO: Run the default set rules
+                for rule in self.set_rules.iter().filter(|x| x.is_default) {
+                    run_commands(rule.commands.clone());
+                }
                 continue;
             }
 
             for set_rule in rules_to_run {
-                // TODO: Run the set rules (run)
-                Logger::info(set_rule);
-                continue;
+                if let Some(rule) = self.set_rules.iter().find(|x| x.name == set_rule) {
+                    run_commands(rule.commands.clone());
+                }
             }
         }
 
