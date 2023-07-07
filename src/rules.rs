6b1c474f3095a7c86566097a8913b4f507280b7c
André Luis <andrescalisejr@gmail.com>
2023-07-06 22:35:44 -0300

    feat: Add define_rule

--- src/rules.rs
+++ src/rules.rs
@@ -1,34 +1,13 @@
 pub mod define_rule;
 
-use std::{io, process::Command};
-
-use colored::Colorize;
-
-use crate::logger::Logger;
-
-trait Runner {
-    fn run(&self);
-}
-
-impl Runner for define_rule::Rule {
-    fn run(&self) {
-        for command in self.commands.iter() {
-            Logger::info(&format!("Running {}", command.purple().bold()));
-
-            match Command::new("sh").arg("-c").arg(command).output() {
-                Ok(o) => Logger::print(&String::from_utf8_lossy(if !o.stdout.is_empty() { &o.stdout } else { &o.stderr })),
-                Err(e) => Logger::error(&e.to_string()),
-            }
-        }
-    }
-}
+use std::io;
 
 pub struct Ruler {
     define_rules: Vec<define_rule::Rule>,
 }
 
 impl Ruler {
-    pub fn new(content: String) -> io::Result<()> {
+    pub fn new(_content: String) -> io::Result<()> {
         Ok(())
     }
 }
